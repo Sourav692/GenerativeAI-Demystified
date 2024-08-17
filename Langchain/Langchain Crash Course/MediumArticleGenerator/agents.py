7eb7d726d671e796853ad84977ea89167bba1e26
Sourav Banerjee <30810740+Sourav692@users.noreply.github.com>
2024-08-17 18:54:57 +0530

    Initial Commit. Added Section for RAG and Langchain

---
+++ Langchain/Langchain Crash Course/MediumArticleGenerator/agents.py
@@ -0,0 +1,17 @@
+import os
+from apikey import apikey
+
+from langchain.llms import OpenAI
+from langchain.agents import load_tools, initialize_agent, AgentType
+
+os.environ["OPENAI_API_KEY"] = apikey
+
+llm = OpenAI(temperature=0.0)
+
+tools = load_tools(['wikipedia','llm-math'], llm=llm)
+
+agent = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
+
+prompt = input('Wikipedia Research Task')
+
+agent.run(prompt)
